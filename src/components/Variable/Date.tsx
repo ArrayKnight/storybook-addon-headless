36cffd6d9e7fa369fb68e7a737cc0f74258038f9
Ray Knight <array.knight+github@gmail.com>
2020-01-25 23:43:13 -0800

    feat: 🎸 create datetime getter/setter

--- src/components/Variable/Date.tsx
+++ src/components/Variable/Date.tsx
@@ -1,5 +1,5 @@
 import { Form } from '@storybook/components'
-// import { formatISO, parseISO } from 'date-fns'
+import { formatISO, parseISO } from 'date-fns'
 import React, { ChangeEvent, memo } from 'react'
 
 import { DateTimeSchema } from '../../types'
@@ -19,38 +19,60 @@ export const DateTimeInput = memo(
         const includeTime = schema.format.endsWith('time')
         const isDate = includeDate && !includeTime
         const isDateTime = includeDate && includeTime
-        // const isTime = !includeDate && includeTime
+        const isTime = !includeDate && includeTime
 
         function getType(): string {
             return isDateTime ? 'datetime-local' : isDate ? 'date' : 'time'
         }
 
+        function getDate(val: string, utc: boolean): Date {
+            if (isTime) {
+                const [hours, minutes] = val.split(':')
+                const date = new Date()
+
+                date[utc ? 'setUTCHours' : 'setHours'](
+                    parseInt(hours, 10),
+                    parseInt(minutes, 10),
+                    0,
+                    0,
+                )
+
+                return date
+            }
+
+            return parseISO(val)
+        }
+
         function getter(val: string): string {
-            if (!!val) {
+            if (!val) {
                 return val
             }
 
-            return val
-        }
-
-        function setter(val: string): string {
-            /*const date = isTime ? new Date() : parseISO(val)
+            const date = getDate(val, true)
             const representation = isDateTime
                 ? 'complete'
                 : isDate
                 ? 'date'
                 : 'time'
 
-            if (isTime) {
-                const [hours, minutes] = val.split(':')
+            return formatISO(date, { representation }).replace(
+                /-\d{2}:\d{2}/,
+                '',
+            )
+        }
 
-                date.setHours(parseInt(hours, 10))
-                date.setMinutes(parseInt(minutes, 10))
+        function setter(val: string): string {
+            const date = getDate(val, false)
+            const iso = date.toISOString()
+
+            if (isDateTime) {
+                return iso
             }
 
-            return formatISO(date, { representation })*/
+            const match = iso.match(/(\d{4}(?:-\d{2}){2})T(\d{2}:\d{2})/)
+            const [, day, time] = match
 
-            return val
+            return isDate ? day : time
         }
 
         function update(event: ChangeEvent<HTMLInputElement>): void {
